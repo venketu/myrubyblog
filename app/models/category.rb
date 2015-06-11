9e505d792cb8a72e28bee536b5af518048d8f50c
Michael Weaver <venketu@yahoo.com>
2015-06-10 23:42:24 -0500

    rehashed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-	attr_accessible :name
+	#attr_accessible :name
 	has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	attr_accessible :title,	:body, :category_id, :author_id
+	#attr_accessible :title,	:body, :category_id, :author_id
 	belongs_to :category
 end
 
